6bbabeee99272e2216dcb640aed3fddfc9889074
tsalitsakir <tsalitsakir@gmail.com>
2025-12-09 17:45:39 +0700

    Create 3.py

---
+++ jawaban ujian algrotima pemrograman python/3.py
@@ -0,0 +1,12 @@
+def terbesar (a, b, c):
+    
+    if a >= b and a >= c:
+        return a
+        
+    elif b >= a and b >= c:
+        return b
+        
+    else:
+        return c
+
+print (terbesar(3, 8, 4)) 
