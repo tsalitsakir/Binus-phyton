49472785e78751a2b2e4759f47f2bbd5676eefe9
tsalitsakir <tsalitsakir@gmail.com>
2025-12-09 17:38:26 +0700

    Create 2.py

---
+++ pertemuan 2/jawaban ujian algortima/2.py
@@ -0,0 +1,14 @@
+a. variabel adalah tempat ubtuk menyimpan data. data dalam variabel ini dapat diubah2 dan versatile. 
+b. y2h_, _t, x
+c.  y2h = 0
+_t = 18
+
+for x in range(5):
+    if x % 2 = 1:
+        y2h = _t - x
+    elif x != 0 and _t > x:
+        _t = _t // x
+    else:
+        y2h = y2h + 1
+
+print(x, y2h)
