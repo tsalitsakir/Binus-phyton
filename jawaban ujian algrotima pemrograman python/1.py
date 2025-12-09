0e8855de81f31afcc8431a309ec11c3a5f664198
tsalitsakir <tsalitsakir@gmail.com>
2025-12-09 17:44:56 +0700

    Create 1.py

---
+++ jawaban ujian algrotima pemrograman python/1.py
@@ -0,0 +1,7 @@
+a = int(input("Masukkan nilai a: "))
+b = int(input("Masukkan nilai b: "))
+v = int(input("Masukkan nilai v: "))
+
+hasil = (b - 10) / (2 * (1 - (a / (2 * v**2))))
+
+print("Hasil y =", hasil)
